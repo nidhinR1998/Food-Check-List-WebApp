7154c84673411c847bb84d1a11b1e7ef8e97c593
nidhinR1998 <nidhinrajesh1998@gmail.com>
2024-07-20 01:57:24 +0530

    Updated the Email Content for both remainder and Reset Password.

--- my-springboot-project-master/src/main/java/com/springbootfproject/firstWebApp/service/ScheduledTaskService.java
+++ my-springboot-project-master/src/main/java/com/springbootfproject/firstWebApp/service/ScheduledTaskService.java
@@ -20,43 +20,85 @@ public class ScheduledTaskService {
     @Autowired
     private UserRepository userRepository;
      
-//   @Scheduled(cron = "0 */2 * * * ?") // This runs every 2 minutes
+ //  @Scheduled(cron = "0 */2 * * * ?") // This runs every 2 minutes
 
-   @Scheduled(cron = "0 0 21 * * ?") // Runs every day at 9 PM
+   @Scheduled(cron = "0 0 17 * * ?") // Runs every day at 5 PM
    public void sendDailyFoodUpdateReminder() {
 	    List<User> users = userRepository.findAll(); 
 	    String appLink = baseUrl;
 	    String subject = "Reminder: Update Your Food Track";
+	   // String subject = "Invitation: Register for Food Check List Application";
 
 	    for (User user : users) {
 	        String email = user.getEmail();
 	        String fullName = user.getFullname();
 	        String htmlContent = "<html>"
-	                            + "<head><style>"
-	                            + "body { font-family: 'Arial', sans-serif; margin: 0; padding: 0; background-color: #f4f4f4; color: #333; }"
-	                            + ".container { width: 80%; margin: auto; background: #fff; padding: 20px; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); }"
-	                            + "h1 { color: #4CAF50; font-size: 24px; }"
-	                            + "p { font-size: 16px; line-height: 1.5; margin: 10px 0; }"
-	                            + "a { color: #4CAF50; text-decoration: none; font-weight: bold; }"
-	                            + "a:hover { text-decoration: underline; }"
-	                            + "footer { font-size: 14px; color: #777; margin-top: 20px; }"
-	                            + "</style></head>"
-	                            + "<body>"
-	                            + "<div class='container'>"
-	                            + "<h1>Hi <strong>" + fullName + "</strong>,</h1>"
-	                            + "<p>Please update your food track for today on the following link:</p>"
-	                            + "<p><a href=\"" + appLink + "\">Food Check List</a></p>"
-	                            + "<p><em>If you have already updated your food track, please ignore this email.</em></p>"
-	                            + "<p><em><strong>Note:</strong> This is an automated message. Please do not reply to this email.</em></p>"
-	                            + "<footer>"
-	                            + "<p>Thanks and regards,</p>"
-	                            + "<p>Your Food Check List Team</p>"
-	                            + "<p>Have a nice day!</p>"
-	                            + "</footer>"
-	                            + "</div>"
-	                            + "</body></html>";
-
+                    + "<head><style>"
+                    + "body { font-family: 'Arial', sans-serif; margin: 0; padding: 0; background-color: #f4f4f4; color: #333; }"
+                    + ".container { width: 80%; margin: auto; background: #fff; padding: 20px; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); position: relative; }"
+                    + ".container::before { content: ''; position: absolute; top: 0; left: 0; width: 100%; height: 100%; background: url('https://www.transparenttextures.com/patterns/scream.png'); opacity: 0.2; z-index: -1; }"
+                    + "h1 { color: #4CAF50; font-size: 24px; }"
+                    + ".reminder-tag { background-color: #ff5722; color: #fff; font-size: 14px; padding: 5px 10px; border-radius: 3px; display: inline-block; margin-bottom: 20px; font-weight: bold; }"
+                    + "p { font-size: 16px; line-height: 1.5; margin: 10px 0; font-weight: bold; }"
+                    + "a { color: #ff9800; background-color: #ff9800; padding: 10px 20px; text-decoration: none; font-weight: bold; border-radius: 5px; display: inline-block; font-size: 18px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); transition: box-shadow 0.3s ease-in-out; }"
+                    + "a:hover { background-color: #ffb74d; box-shadow: 0 6px 12px rgba(0, 0, 0, 0.3); }"
+                    + ".highlight { background-color: #eaf3fc; padding: 10px; border-radius: 5px; }"
+                    + ".info-box { border: 1px solid #eaf3fc; padding: 10px; border-radius: 5px; background-color: #f9f9f9; }"
+                    + ".info-box p { margin: 5px 0; }"
+                    + ".info-box strong { color: #4CAF50; }"
+                    + ".highlight p { font-style: italic; color: #ff9800; }"
+                    + "footer { font-size: 14px; color: #777; margin-top: 20px; font-weight: bold; text-align: left; }"
+                    + "</style></head>"
+                    + "<body>"
+                    + "<div class='container'>"
+                    + "<h1>Hi <strong>" + fullName + "</strong>,</h1>"
+                    + "<span class='reminder-tag'>Reminder</span>"
+                    + "<p>Please update your food track for today on the following link:</p>"
+                    + "<p><a href=\"" + appLink + "\">Food Check List</a></p>"
+                    + "<div class='info-box'>"
+                    + "<p>If you have already updated your food track, please ignore this email.</p>"
+                    + "<p class='highlight'><em><strong>Note:</strong> This is an automated message. Please do not reply to this email.</em></p>"                    
+                    + "</div>"
+                    + "<footer>"
+                    + "<p>Thanks and regards,</p>"
+                    + "<p>Your Food Check List Team</p>"
+                    + "<p>Have a nice day!</p>"
+                    + "</footer>"
+                    + "</div>"
+                    + "</body></html>";
+	        
+	        
 	        emailService.sendEmail(email, subject, htmlContent);
+	        
+			/* For New registration Mail */
+	        
+			/*
+			 * String htmlContent ="<html>" + "<head><style>" +
+			 * "body { font-family: 'Arial', sans-serif; margin: 0; padding: 0; background-color: #f4f4f4; color: #333; }"
+			 * +
+			 * ".container { width: 80%; margin: auto; background: #fff; padding: 20px; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); }"
+			 * + "h1 { color: #4CAF50; font-size: 24px; }" +
+			 * "p { font-size: 16px; line-height: 1.5; margin: 10px 0; font-weight: bold; }"
+			 * +
+			 * "a { color: #fff; background-color: #5bc0de; padding: 10px 20px; text-decoration: none; font-weight: bold; border-radius: 5px; display: inline-block; font-size: 18px; }"
+			 * + "a:hover { background-color: #31b0d5; }" +
+			 * ".highlight { background-color: #eaf3fc; padding: 10px; border-radius: 5px; }"
+			 * +
+			 * "footer { font-size: 14px; color: #777; margin-top: 20px; font-weight: bold; }"
+			 * + "</style></head>" + "<body>" + "<div class='container'>" +
+			 * "<h1>Hi <strong>" + fullName + "</strong>,</h1>" +
+			 * "<p>We are excited to invite you to register for our <strong>Food Check List</strong> application.</p>"
+			 * +
+			 * "<p>You can add your daily food details along with the cost using the following link:</p>"
+			 * + "<p><a href=\"" + appLink + "\">Food Check List</a></p>" +
+			 * "<p class='highlight'><em>Please note that you will receive a reminder email every day at 9 PM to fill in your daily food details.</em></p>"
+			 * +
+			 * "<p><em><strong>Note:</strong> This is an automated message. Please do not reply to this email.</em></p>"
+			 * + "<footer>" + "<p>Thanks and regards,</p>" +
+			 * "<p>Your Food Check List Team</p>" + "<p>Have a nice day!</p>" + "</footer>"
+			 * + "</div>" + "</body></html>";
+			 */
+	       
 	    }
 	}
 }
--- my-springboot-project-master/src/main/java/com/springbootfproject/firstWebApp/service/UserServiceImpl.java
+++ my-springboot-project-master/src/main/java/com/springbootfproject/firstWebApp/service/UserServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.mail.MailAuthenticationException;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -18,9 +19,12 @@ import com.springbootfproject.firstWebApp.dto.UserDto;
 import com.springbootfproject.firstWebApp.repository.UserRepository;
 import com.springbootfproject.firstWebApp.todomodel.User;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
 @Service
 public class UserServiceImpl implements UserService {
-	
+
 	@Value("${application.base-url}")
 	private String baseUrl;
 
@@ -51,7 +55,7 @@ public class UserServiceImpl implements UserService {
 	public User save2(UserDto userDto) {
 		logger.debug("Saving user: {}", userDto.getUsername());
 		User user = new User(userDto.getUsername(), passwordEncoder.encode(userDto.getPassword()),
-				userDto.getFullname(),userDto.getEmail(), userDto.getSecurityQuestion(), userDto.getSecurityAnswer(),
+				userDto.getFullname(), userDto.getEmail(), userDto.getSecurityQuestion(), userDto.getSecurityAnswer(),
 				userDto.getResetToken());
 		return userRepository.save(user);
 	}
@@ -60,8 +64,30 @@ public class UserServiceImpl implements UserService {
 	public User save(UserDto userDto) {
 		logger.debug("Saving user: {}", userDto.getUsername());
 		User user = new User(userDto.getUsername(), passwordEncoder.encode(userDto.getPassword()),
-				userDto.getFullname(),userDto.getEmail(), userDto.getSecurityQuestion(), userDto.getSecurityAnswer(),
+				userDto.getFullname(), userDto.getEmail(), userDto.getSecurityQuestion(), userDto.getSecurityAnswer(),
 				userDto.getResetToken());
+		// Otp validation
+		int otp =  (int) (Math.random() * 9000) + 1000;
+		logger.debug("The OTP: {}", otp);
+		SimpleMailMessage message = new SimpleMailMessage();
+		if (isEmailValid(user.getEmail())) {
+			message.setTo(user.getEmail());
+		} else {
+			logger.error("Invalid email address: " + user.getEmail());
+			
+		}
+		message.setSubject("OTP VALIDATION");
+		message.setText("Hello \n\n" + "Your Login OTP :" + otp + ".Please Verify. \n\n" + "Regards \n" + "Your Food Check List Team");
+		try {
+			mailSender.send(message);
+			logger.debug("OTP is generated and email sent to user: {}", userDto.getUsername());	
+			
+		} catch (MailAuthenticationException ex) {
+			logger.error("Mail authentication failed", ex);
+		} catch (MailException ex) {
+			logger.error("Failed to send email", ex);
+		}
+
 		return userRepository.save(user);
 	}
 
@@ -75,23 +101,57 @@ public class UserServiceImpl implements UserService {
 	        user.setResetToken(resetToken);
 	        userRepository.save(user);
 	        String resetUrl = baseUrl + resetPasswordPath + "?token=" + resetToken;
-	        SimpleMailMessage message = new SimpleMailMessage();
-	        if (isEmailValid(user.getEmail())) {
-	            message.setTo(user.getEmail());
-	        } else {
-	            logger.error("Invalid email address: " + user.getEmail());
-	            return false;
-	        }
-	        message.setTo(user.getEmail());
-	        message.setSubject("Password Reset Request");
-	        message.setText("Click the link to reset your password: " + resetUrl);
+
+	        String htmlContent = "<html>"
+	                            + "<head><style>"
+	                            + "body { font-family: 'Arial', sans-serif; margin: 0; padding: 0; background-color: #f4f4f4; color: #333; }"
+	                            + ".container { width: 80%; margin: auto; background: #fff; padding: 20px; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); position: relative; }"
+	                            + ".container::before { content: ''; position: absolute; top: 0; left: 0; width: 100%; height: 100%; background: url('https://www.transparenttextures.com/patterns/scream.png'); opacity: 0.2; z-index: -1; }"
+	                            + "h1 { color: #4CAF50; font-size: 24px; }"
+	                            + ".reminder-tag { background-color: #ff5722; color: #fff; font-size: 14px; padding: 5px 10px; border-radius: 3px; display: inline-block; margin-bottom: 20px; font-weight: bold; }"
+	                            + "p { font-size: 16px; line-height: 1.5; margin: 10px 0; font-weight: bold; }"
+	                            + "a { color: #ff9800; background-color: #ff9800; padding: 10px 20px; text-decoration: none; font-weight: bold; border-radius: 5px; display: inline-block; font-size: 18px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); transition: box-shadow 0.3s ease-in-out; }"
+	                            + "a:hover { background-color: #ffb74d; box-shadow: 0 6px 12px rgba(0, 0, 0, 0.3); }"
+	                            + ".highlight { background-color: #eaf3fc; padding: 10px; border-radius: 5px; }"
+	                            + ".info-box { border: 1px solid #eaf3fc; padding: 10px; border-radius: 5px; background-color: #f9f9f9; }"
+	                            + ".info-box p { margin: 5px 0; }"
+	                            + ".info-box strong { color: #4CAF50; }"
+	                            + ".highlight p { font-style: italic; color: #ff9800; }"
+	                            + "footer { font-size: 14px; color: #777; margin-top: 20px; font-weight: bold; text-align: left; }"
+	                            + "</style></head>"
+	                            + "<body>"
+	                            + "<div class='container'>"
+	                            + "<h1>Password Reset Request</h1>"
+	                            + "<p>Hi <strong>" + user.getFullname() + "</strong>,</p>"
+	                            + "<p>We received a request to reset your password. Click the link below to reset your password:</p>"
+	                            + "<p><a href=\"" + resetUrl + "\">Reset Password</a></p>"
+	                            + "<div class='info-box'>"
+	                            + "<p>If you did not request a password reset, please ignore this email.</p>"
+	                            + "<p class='highlight'><em><strong>Note:</strong> This is an automated message. Please do not reply to this email.</em></p>"
+	                            + "</div>"
+	                            + "<footer>"
+	                            + "<p>Thanks and regards,</p>"
+	                            + "<p>Your Food Check List Team</p>"
+	                            + "<p>Have a nice day!</p>"
+	                            + "</footer>"
+	                            + "</div>"
+	                            + "</body></html>";
+
+	        MimeMessage mimeMessage = mailSender.createMimeMessage();
 	        try {
-	            mailSender.send(message);
+	            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+	            if (isEmailValid(user.getEmail())) {
+	                helper.setTo(user.getEmail());
+	            } else {
+	                logger.error("Invalid email address: " + user.getEmail());
+	                return false;
+	            }
+	            helper.setSubject("Password Reset Request");
+	            helper.setText(htmlContent, true);
+	            mailSender.send(mimeMessage);
 	            logger.debug("Password reset token generated and email sent to user: {}", username);
 	            return true;
-	        } catch (MailAuthenticationException ex) {
-	            logger.error("Mail authentication failed", ex);
-	        } catch (MailException ex) {
+	        } catch (MessagingException ex) {
 	            logger.error("Failed to send email", ex);
 	        }
 	    }
@@ -107,27 +167,28 @@ public class UserServiceImpl implements UserService {
 	    return pat.matcher(email).matches();
 	}
 
+
 	@Override
 	public boolean isResetTokenValid(String token) {
-	    logger.debug("Validating reset token: {}", token);
-	    User user = userRepository.findByResetToken(token);
-	    boolean isValid = user != null && token.equals(user.getResetToken());
-	    logger.debug("Is token valid: {}", isValid);
-	    return isValid;
+		logger.debug("Validating reset token: {}", token);
+		User user = userRepository.findByResetToken(token);
+		boolean isValid = user != null && token.equals(user.getResetToken());
+		logger.debug("Is token valid: {}", isValid);
+		return isValid;
 	}
 
 	@Override
 	public void updatePassword(String token, String newPassword) {
-	    logger.debug("Updating password for token: {}", token);
-	    User user = userRepository.findByResetToken(token);
-	    if (user != null) {
-	        user.setPassword(passwordEncoder.encode(newPassword));
-	        user.setResetToken(null); 
-	        userRepository.save(user);
-	        logger.debug("Password updated and reset token cleared for user: {}", user.getUsername());
-	    } else {
-	        logger.debug("User not found for token: {}", token);
-	    }
+		logger.debug("Updating password for token: {}", token);
+		User user = userRepository.findByResetToken(token);
+		if (user != null) {
+			user.setPassword(passwordEncoder.encode(newPassword));
+			user.setResetToken(null);
+			userRepository.save(user);
+			logger.debug("Password updated and reset token cleared for user: {}", user.getUsername());
+		} else {
+			logger.debug("User not found for token: {}", token);
+		}
 	}
 
 	@Override
